405fea4ee009a1ea5c78c065f23795e4b12a8291
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-10-20 22:08:52 +0700

    Video 27 - Type Parametes di Generic Constraint

---
+++ program/26TypeParameters_in_GenericConstraint.ts
@@ -0,0 +1,9 @@
+// Keyof ==> key dari generic pertama
+function getProperty<T, U extends keyof T>(obj: T, key: U) {
+  return obj[key];
+}
+
+let x = { a: 1, b: 2, c: 3, d: 4 };
+console.log(getProperty(x, "a"));
+console.log(getProperty(x, "b"));
+console.log(getProperty(x, "z")); // error, karena keyof harus berada di variabel x ini
